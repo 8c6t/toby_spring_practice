8485b21a342404109969ef7220188d11d5f443ae
8c6t <nkw8620@gmail.com>
2020-09-10 13:51:47 +0900

    1.4 제어의 역전

--- src/main/java/com/hachicore/user/dao/UserDaoTest.java
+++ src/main/java/com/hachicore/user/dao/UserDaoTest.java
@@ -3,7 +3,6 @@ package com.hachicore.user.dao;
 public class UserDaoTest {
 
     public static void main(String[] args) {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
     }
 }

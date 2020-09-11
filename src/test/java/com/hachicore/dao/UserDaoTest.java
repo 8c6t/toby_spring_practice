07e5fea9d2e2ba2d3ca7a8a01b71ceb2295beb6a
8c6t <nkw8620@gmail.com>
2020-09-11 10:18:53 +0900

    2.2 UserDaoTest 개선

--- src/test/java/com/hachicore/dao/UserDaoTest.java
+++ src/test/java/com/hachicore/dao/UserDaoTest.java
@@ -1,14 +1,18 @@
-package com.hachicore.user.dao;
+package com.hachicore.dao;
 
+import com.hachicore.user.dao.UserDao;
 import com.hachicore.user.domain.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-import java.sql.SQLException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class UserDaoTest {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    @Test
+    public void addAndGet() throws Exception {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
@@ -19,13 +23,10 @@ public class UserDaoTest {
 
         dao.add(user);
 
-        System.out.println(user.getId() + "등록 성공");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 
 }

3055e3d2116cdffa7180fda60247de51c14e2457
8c6t <nkw8620@gmail.com>
2020-09-13 08:57:53 +0900

    5.1.5 코드 개선

--- src/test/java/com/hachicore/service/UserServiceTest.java
+++ src/test/java/com/hachicore/service/UserServiceTest.java
@@ -14,6 +14,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.hachicore.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static com.hachicore.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -33,11 +35,11 @@ public class UserServiceTest {
     @Before
     public void setUp() {
         users = Arrays.asList(
-                new User("테스트1", "테스트1", "t1", Level.BASIC, 49, 0),
-                new User("테스트2", "테스트2", "t2", Level.BASIC, 50, 0),
-                new User("테스트3", "테스트2", "t3", Level.SILVER, 60, 29),
-                new User("테스트4", "테스트4", "t4", Level.SILVER, 60, 30),
-                new User("테스트5", "테스트5", "t5", Level.GOLD, 100, 100)
+                new User("테스트1", "테스트1", "t1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER - 1, 0),
+                new User("테스트2", "테스트2", "t2", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
+                new User("테스트3", "테스트2", "t3", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD - 1),
+                new User("테스트4", "테스트4", "t4", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
+                new User("테스트5", "테스트5", "t5", Level.GOLD, 100, Integer.MAX_VALUE)
         );
     }
 
@@ -56,11 +58,11 @@ public class UserServiceTest {
 
         userService.upgradeLevels();
 
-        checkLevel(users.get(0), Level.BASIC);
-        checkLevel(users.get(0), Level.SILVER);
-        checkLevel(users.get(0), Level.SILVER);
-        checkLevel(users.get(0), Level.GOLD);
-        checkLevel(users.get(0), Level.GOLD);
+        checkLevel(users.get(0), false);
+        checkLevel(users.get(0), true);
+        checkLevel(users.get(0), false);
+        checkLevel(users.get(0), true);
+        checkLevel(users.get(0), false);
     }
 
     @Test
@@ -81,8 +83,13 @@ public class UserServiceTest {
         assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
     }
 
-    private void checkLevel(User user, Level expectedLevel) {
+    private void checkLevel(User user, boolean upgraded) {
         User userUpdate = userDao.get(user.getId());
-        assertThat(userUpdate.getLevel(), is(expectedLevel));
+
+        if (upgraded) {
+            assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+        } else {
+            assertThat(userUpdate.getLevel(), is(user.getLevel()));
+        }
     }
 }
